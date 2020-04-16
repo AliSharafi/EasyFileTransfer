bb9a2bbc031fa913602ff32a5805f10e9c24dc97
Ali Sharafi <ali.sharafi@gmail.com>
2020-04-17 00:01:49 +0430

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace EasyFileTransfer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnOpenDialog = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(517, 45);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(186, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(12, 45);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(443, 20);
+            this.txtFileName.TabIndex = 1;
+            // 
+            // btnOpenDialog
+            // 
+            this.btnOpenDialog.Location = new System.Drawing.Point(461, 45);
+            this.btnOpenDialog.Name = "btnOpenDialog";
+            this.btnOpenDialog.Size = new System.Drawing.Size(35, 23);
+            this.btnOpenDialog.TabIndex = 2;
+            this.btnOpenDialog.Text = "...";
+            this.btnOpenDialog.UseVisualStyleBackColor = true;
+            this.btnOpenDialog.Click += new System.EventHandler(this.btnOpenDialog_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(26, 362);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(0, 13);
+            this.lblInfo.TabIndex = 3;
+            // 
+            // btnClose
+            // 
+            this.btnClose.ForeColor = System.Drawing.Color.Red;
+            this.btnClose.Location = new System.Drawing.Point(697, 1);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(35, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(92, 12);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(160, 20);
+            this.txtIP.TabIndex = 5;
+            this.txtIP.TextChanged += new System.EventHandler(this.txtIP_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Destination IP";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(170)))), ((int)(((byte)(225)))));
+            this.ClientSize = new System.Drawing.Size(732, 77);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnOpenDialog);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.btnSend);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmMain";
+            this.Opacity = 0.75D;
+            this.Text = "frmMain";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMain_FormClosed);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnOpenDialog;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ frmMain.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyFileTransfer
+{
+    public partial class frmMain : Form
+    {
+        #region Constants
+        const int _port = 2345;
+        string _serverIP = "192.168.1.8";
+        #endregion
+
+        #region fields
+        string _fName;
+        Thread _listenThread;
+        int _flag = 0;
+        string _receivedPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\";
+        public delegate void ReceiveDelegate();
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+        #endregion
+        
+        public frmMain()
+        {
+            _listenThread = new Thread(new ThreadStart(StartListening));
+            _listenThread.Start();
+            InitializeComponent();
+        }
+
+        #region Send File
+        private void btnOpenDialog_Click(object sender, EventArgs e)
+        {
+            // Show the open file dialog to select our data.
+            openFileDialog1.ShowDialog();
+
+            //Get the file name and write it into our text box.
+            txtFileName.Text = openFileDialog1.FileName;
+
+            _fName = Path.GetFileName(openFileDialog1.FileName);
+
+            if (txtFileName.Text != null) 
+                btnSend.Enabled = true;
+        }
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            Socket clientSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            byte[] fileName = Encoding.UTF8.GetBytes(_fName); //file name
+            byte[] fileData = File.ReadAllBytes(txtFileName.Text); //file
+            byte[] fileNameLen = BitConverter.GetBytes(fileName.Length); //lenght of file name
+
+            byte[] m_clientData = new byte[4 + fileName.Length + fileData.Length];
+
+            fileNameLen.CopyTo(m_clientData, 0);
+            fileName.CopyTo(m_clientData, 4);
+            fileData.CopyTo(m_clientData, 4 + fileName.Length);
+
+            try
+            {
+                clientSock.Connect(_serverIP, _port); //target machine's ip address and the port number
+                clientSock.Send(m_clientData);
+                clientSock.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Msg No : 1   Could not connect", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+        #endregion
+
+        #region Receive file
+        private void StartListening()
+        {
+            //byte[] bytes = new Byte[1024];
+            IPEndPoint ipEnd = new IPEndPoint(IPAddress.Any, _port);
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                listener.Bind(ipEnd);
+                listener.Listen(32);
+                while (true)
+                {
+                    allDone.Reset();
+                    listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
+                    allDone.WaitOne();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        public void AcceptCallback(IAsyncResult ar)
+        {
+            allDone.Set();
+
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            StateObject state = new StateObject();
+            state.workSocket = handler;
+            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+            new AsyncCallback(ReadCallback), state);
+            _flag = 0;
+        }
+        public void ReadCallback(IAsyncResult ar)
+        {
+            int fileNameLen = 1;
+            String content = String.Empty;
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+            int bytesRead = handler.EndReceive(ar);
+            if (bytesRead > 0)
+            {
+                if (_flag == 0)
+                {
+                    fileNameLen = BitConverter.ToInt32(state.buffer, 0);
+                    string fileName = Encoding.UTF8.GetString(state.buffer, 4, fileNameLen);
+                    _receivedPath += fileName;
+                    _flag++;
+                }
+                if (_flag >= 1)
+                {
+                    BinaryWriter writer = new BinaryWriter(File.Open(_receivedPath, FileMode.Append));
+                    if (_flag == 1)
+                    {
+                        writer.Write(state.buffer, 4 + fileNameLen, bytesRead - (4 + fileNameLen));
+                        _flag++;
+                    }
+                    else
+                        writer.Write(state.buffer, 0, bytesRead);
+                    writer.Close();
+                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                    new AsyncCallback(ReadCallback), state);
+                }
+            }
+            else
+            {
+                Invoke(new ReceiveDelegate(LabelWriter));
+            }
+        }
+        public void LabelWriter()
+        {
+            lblInfo.Text = "Data has been received";
+        }
+        private void frmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _listenThread.Abort();
+        }
+        #endregion
+
+        #region Make form draggable
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        private void frmMain_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        #endregion
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txtIP_TextChanged(object sender, EventArgs e)
+        {
+            _serverIP = txtIP.Text;
+        }
+    }
+}
